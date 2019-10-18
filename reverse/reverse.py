94e9e64ad1c2086c49e795335da04b0c533e637a
Adam Mathieson <mathieson1222@gmail.com>
2019-10-18 08:36:47 -0700

    adds test list to file

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -43,5 +43,16 @@ class LinkedList:
     return False
 
   def reverse_list(self):
-    # TO BE COMPLETED
     pass
+
+    test = LinkedList()
+    test.add_to_head(1)
+    test.add_to_head(2)
+    test.add_to_head(3)
+    test.add_to_head(4)
+    test.add_to_head(5)
+
+    print(test.head.value)
+    # test.reverse_list()
+    print(test.head.value)
+    
