c0b08894e96d66c4ef721b7b25dd6019e4f06ca2
Adam Mathieson <mathieson1222@gmail.com>
2019-10-18 09:12:56 -0700

    append is working aboutexpected, not tested yet - will write .get first

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,31 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    print('current--->', self.current)
+    print('storage--->', self.storage)
+    self.storage[self.current] = item
+
+    if self.current < self.capacity - 1:
+      self.current += 1
+    else:
+      self.current = 0
+      # self.storage = self.storage[1:]
+      # self.storage += [item]
+    # else:
+    #   self.storage.append(item)
 
   def get(self):
     pass
+
+
+
+test = RingBuffer(3)
+
+# test.storage.append('something')
+
+test.append('test')
+test.append('test2')
+test.append('test3')
+test.append('test4')
+
+print(test.storage)
